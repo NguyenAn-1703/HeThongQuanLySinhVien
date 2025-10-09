c03141a53747ce5063c566543ebecdbc049472e8
Giabao249 <nguyenaugiabao24905@gmail.com>
2025-10-09 23:33:32 +0700

    update them sinh vien

--- QuanLySinhVien/Views/Components/NavList/SinhVien.cs
+++ QuanLySinhVien/Views/Components/NavList/SinhVien.cs
@@ -1,10 +1,10 @@
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing.Drawing2D;
 using QuanLySinhVien.Views.Components.CommonUse;
 using QuanLySinhVien.Views.Components.NavList;
 using QuanLySinhVien.Views.Enums;
 using Svg;
-
 namespace QuanLySinhVien.Views.Components;
 
 public class SinhVien : NavBase
@@ -40,6 +40,7 @@ public class SinhVien : NavBase
             Font = GetFont.GetFont.GetMainFont(10, FontType.Bold),
             Dock = DockStyle.Right,
             TextAlign = ContentAlignment.TopRight,
+            Anchor = AnchorStyles.None
             // Width = 300,
             // AutoSize = true,
             // Padding = new  Padding(50 , 0 , 0 , 0),
@@ -56,7 +57,7 @@ public class SinhVien : NavBase
             Dock = DockStyle.Top,
             BackColor = ColorTranslator.FromHtml("#E5E7EB"),
             //BackColor = Color.Red,
-            Height = 120,
+            Height = 100,
         };
         var header = new TableLayoutPanel
         {
@@ -95,29 +96,76 @@ public class SinhVien : NavBase
             Dock = DockStyle.Fill,
             // BackColor = Color.Red,
         };
-
+        var textLabel = new Label
+        {
+            Text = "Thêm sinh viên",
+            TextAlign = ContentAlignment.MiddleCenter,
+            Font = GetFont.GetFont.GetMainFont(13, FontType.Bold),
+            Width = 300,
+            Height = 60,
+            Dock = DockStyle.Fill,
+        };
         var topDialog = new Panel()
         {
             BackColor = Color.Aqua,
             Dock = DockStyle.Fill,
-            // Height = 90,
+            // Height = 90
         };
+        topDialog.Controls.Add(textLabel);
         
+        var middleDialogFull = new TableLayoutPanel
+        {
+            ColumnCount = 1,
+            RowCount = 2,
+            Dock = DockStyle.Fill,
+        };
+
+        var imgMiddleDialog = new TableLayoutPanel
+        {
+            Dock = DockStyle.Fill,
+            // BackColor = Color.Yellow,
+            RowCount = 2,
+        };
         var middleDialog = new Panel()
         {
-            // BackColor = Color.Brown,
             Dock = DockStyle.Fill,
-            // AutoSize = true,
-            // Height = 410,
+        };
+        var pathImgUpload = Path.Combine(AppContext.BaseDirectory, "img" , "upload.svg");
+        var imgUpload = SvgDocument.Open(pathImgUpload).Draw(250, 250);
+        var boxImg = new PictureBox
+        {   
+            Image = imgUpload,
+            //BackColor = Color.Blue,
+            Anchor = AnchorStyles.None,
+            Width = 250,
+            Height = 250,
         };
         
-
+        var imgAddButton = new Button
+        {
+            Text = "Thêm ảnh",
+            Anchor = AnchorStyles.None,
+            Font = GetFont.GetFont.GetMainFont(12, FontType.Bold),
+            ImageAlign = ContentAlignment.MiddleCenter,
+            Width = 200,
+            Height = 70,
+        };  
+        imgMiddleDialog.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+        imgMiddleDialog.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+        imgMiddleDialog.Controls.Add(imgAddButton , 0 , 0);
+        imgMiddleDialog.Controls.Add(boxImg , 0 , 1);
+        
+        // var deleteImgButton = new Button
+        
+        middleDialogFull.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
+        middleDialogFull.RowStyles.Add(new RowStyle(SizeType.Percent, 60F));
         var borderMiddleLeft = new TableLayoutPanel()
         {
             ColumnCount = 1,
             RowCount = 5,   
             // BackColor = Color.Black,
             Dock = DockStyle.Fill,
+            // Padding = new Padding(0, 10, 0, 0),
         };
         
         var borderMiddleRight = new TableLayoutPanel()
@@ -134,49 +182,184 @@ public class SinhVien : NavBase
             RowCount = 1,
             // BackColor = Color.Aqua,
             Dock = DockStyle.Fill,
-            Padding = new Padding(10 , 0 , 0 , 10),
+            // Padding = new Padding(10 , 0 , 0 , 10),
         };
-        borderMiddle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        borderMiddle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
         borderMiddle.Controls.Add(borderMiddleLeft , 0 , 0);
-        borderMiddle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        borderMiddle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 65F));
         borderMiddle.Controls.Add(borderMiddleRight , 1 , 0);
-        borderMiddle.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+        // borderMiddle.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
         middleDialog.Controls.Add(borderMiddle);
-        var bottomDialog = new Panel()
+        
+        middleDialogFull.Controls.Add(imgMiddleDialog , 0 , 0);
+        middleDialogFull.Controls.Add(middleDialog , 0 , 1);
+        
+        
+        var bottomDialog = new TableLayoutPanel()
         {
-            // Height = 100,
-            BackColor = Color.Lime,
             Dock = DockStyle.Fill,
+            ColumnCount = 3,
+            RowCount = 1,
         };
-        borderMiddleLeft.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-        for (int i = 1; i <= 5; i++)
+        bottomDialog.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33f));
+        bottomDialog.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33f));
+        bottomDialog.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33f));
+        var cancelButton = new Button
         {
-            borderMiddleLeft.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            borderMiddleRight.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            Text = "Hủy",
+            TextAlign = ContentAlignment.MiddleCenter,
+            FlatStyle = FlatStyle.Flat,
+            BackColor = Color.Red,
+            Cursor = Cursors.Hand,
+            // Dock = DockStyle.Left,
+            Font = GetFont.GetFont.GetMainFont(10, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            Width = 140,
+            Height = 50,
+        };
+        var addButton = new Button
+        {
+            Text = "Thêm",
+            TextAlign = ContentAlignment.MiddleCenter,
+            FlatStyle = FlatStyle.Flat,
+            BackColor = Color.Green,
+            Cursor = Cursors.Hand,
+            // Dock = DockStyle.Right,
+            Font = GetFont.GetFont.GetMainFont(10, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            Width = 140,
+            Height = 50,
+        };
+        var resetButton = new Button
+        {
+            Text = "Làm mới",
+            TextAlign = ContentAlignment.MiddleCenter,
+            FlatStyle = FlatStyle.Flat,
+            BackColor = Color.Cyan,
+            Cursor = Cursors.Hand,
+            // Dock = DockStyle.Fill,
+            Font = GetFont.GetFont.GetMainFont(10, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            Width = 140,
+            Height = 50,
+        };
+        cancelButton.Margin = new Padding(20, 0, 20, 0);
+        resetButton.Margin = new Padding(20, 0, 20, 0);
+        addButton.Margin   = new Padding(20, 0, 20, 0);
+        bottomDialog.Controls.Add(cancelButton);   
+        bottomDialog.Controls.Add(resetButton);   
+        bottomDialog.Controls.Add(addButton);   
+        //borderMiddleLeft.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+        List<string> list = new List<string>{"Mã Sinh Viên: " ,"Tên Sinh Viên: " , "Ngày sinh: " , "Khoa: " , "Giới tính: "};
+        float tile = 60f / list.Count;
+        string[] cbb = new []{ "Cộng nghệ thông tin" };
+        List<Control> rightComponents = new List<Control>();
+        var radioNam = new RadioButton
+        {
+            Text = "Nam",
+            Anchor = AnchorStyles.None,
+            TextAlign = ContentAlignment.MiddleCenter,
+            AutoSize = true
+        };
+        var radioNu = new RadioButton
+        {
+            Text = "Nữ",
+            Anchor = AnchorStyles.None,
+            TextAlign = ContentAlignment.MiddleCenter,
+            AutoSize = true
+        };
+        var tableRadioButton = new TableLayoutPanel()
+        {
+            Dock = DockStyle.Fill,
+            ColumnCount = 2,
+            RowCount = 1,
+        };
+        tableRadioButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        tableRadioButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        tableRadioButton.Controls.Add(radioNam, 0, 0);
+        tableRadioButton.Controls.Add(radioNu, 1, 0);
+        
+        var panelRadioButton = new Panel()
+        {
+            Dock = DockStyle.Fill,
+            Width = 300,
+            Font = GetFont.GetFont.GetMainFont(11, FontType.Regular),
+            // BackColor = Color.Red,
+            Anchor = AnchorStyles.None,
+        };
+        panelRadioButton.Controls.Add(tableRadioButton);
+        var combo = new ComboBox()
+        {
+            DropDownStyle = ComboBoxStyle.DropDownList,
+            Width = 300,
+            Font = GetFont.GetFont.GetMainFont(11, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            // Margin = new Padding(0 , 0 , 0 , 0),
+        };
+        combo.Items.AddRange(cbb);
+        rightComponents.Add(new TextBox()
+        {
+            BorderStyle = BorderStyle.FixedSingle,
+            Width = 300,
+            Height = 100,
+            Font = GetFont.GetFont.GetMainFont(11, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            // Margin = new Padding(5),
+        });
+        rightComponents.Add(new TextBox()
+        {
+            BorderStyle = BorderStyle.FixedSingle,
+            Width = 300,
+            Height = 100,
+            Font = GetFont.GetFont.GetMainFont(11, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            //Margin = new Padding(5),
+        });
+        rightComponents.Add(new DateTimePicker()
+        {
+            Value = DateTime.Now,
+            Width = 300,
+            Height = 100,
+            Font = GetFont.GetFont.GetMainFont(10, FontType.Regular),
+            Anchor = AnchorStyles.None,
+            //Margin = new Padding(5),
+        });
+        rightComponents.Add(combo);
+        rightComponents.Add(panelRadioButton);
+        
+        
+        // Console.WriteLine(tile);
+        for (int i = 1; i <= list.Count; i++)
+        {
+            borderMiddleLeft.RowStyles.Add(new RowStyle(SizeType.Percent,  tile));
+            borderMiddleRight.RowStyles.Add(new RowStyle(SizeType.Percent, tile));
         }
-        List<string> list = new List<string>{"Mã Sinh Viên: " ,"Tên Sinh Viên: " , "Ngày sinh: " , "Khoa: " , "...: " };
-        for (int i = 0; i < 5; i++)
+        
+        for (int i = 0; i < list.Count; i++)
         {
             Label lb = JLable(list[i]);
+            Control rb = rightComponents[i];
             borderMiddleLeft.Controls.Add(lb , 0 , i);
-            
+            borderMiddleRight.Controls.Add(rb , 1 , i);
         }
+        
         fullDialog.Controls.Add(topDialog , 0 , 0);
-        fullDialog.Controls.Add(middleDialog , 0 , 1);
+        fullDialog.Controls.Add(middleDialogFull , 0 , 1);
         fullDialog.Controls.Add(bottomDialog , 0 , 2);
         
         fullDialog.RowStyles.Clear();
+        fullDialog.RowStyles.Add(new RowStyle(SizeType.Percent , 5F));
+        fullDialog.RowStyles.Add(new RowStyle(SizeType.Percent , 80F));
         fullDialog.RowStyles.Add(new RowStyle(SizeType.Percent , 15F));
-        fullDialog.RowStyles.Add(new RowStyle(SizeType.Percent , 70F));
-        fullDialog.RowStyles.Add(new RowStyle(SizeType.AutoSize));
         
         btnAdd.Click += (s , e) =>
         {
             Form form = new Form()
             {
                 Text = "Thêm sinh viên",
-                Size = new Size(600, 600),
+                Size = new Size(600, 1000),
                 StartPosition = FormStartPosition.CenterParent,
+                FormBorderStyle = FormBorderStyle.None,
                 Controls = { fullDialog }
             };
             form.ShowDialog();
@@ -185,7 +368,7 @@ public class SinhVien : NavBase
         var textNavList = new Label
         {
             Text = "Sinh Viên",
-            // Font = new Font("JetBrains Mono", 16f , FontStyle.Bold),
+            Font = GetFont.GetFont.GetMainFont(15, FontType.Bold),
             Size = new Size(200, 40),
             UseCompatibleTextRendering = false,
         };
@@ -227,7 +410,7 @@ public class SinhVien : NavBase
         // add row table
         table.Columns.Add("Mã SV", typeof(string));
         table.Columns.Add("Tên Sinh Viên", typeof(string));
-        table.Columns.Add("Ngày Sinh", typeof(string));
+        table.Columns.Add("Ngày sinh", typeof(string));
         table.Columns.Add("Giới tính", typeof(string));
         table.Columns.Add("Ngành", typeof(string));
         table.Columns.Add("Trạng thái", typeof(string));
@@ -247,6 +430,7 @@ public class SinhVien : NavBase
         dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.None;
         dataGridView.RowHeadersVisible = false;
         dataGridView.Dock = DockStyle.Fill;
+        dataGridView.Font = GetFont.GetFont.GetMainFont(10, FontType.Regular);
         // change Width
         bool _actionsAdded = false;
         dataGridView.DataBindingComplete += (s, e) =>
@@ -315,6 +499,7 @@ public class SinhVien : NavBase
         {
             BackColor = ColorTranslator.FromHtml("#07689F"),
             ForeColor = ColorTranslator.FromHtml("#f5f5f5"),
+            Font = GetFont.GetFont.GetMainFont(11, FontType.Bold),
             //ForeColor = Color.White,
             // Font = new Font("JetBrains Mono", 10f, FontStyle.Bold),
             Alignment = DataGridViewContentAlignment.MiddleCenter,
@@ -325,5 +510,4 @@ public class SinhVien : NavBase
     {
         return ConvertArray_ListString.ConvertArrayToListString(this._listSelectionForComboBox);
     }
-    
 }
